e91642b6a653e55cc7fac67e4375a68bd0be7b4d
Eri Irawan <email": null>
2017-02-28 13:46:29 +0700

    update url for user_timeline.json

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const bodyParser = require('body-parser')
 const app = express()
 
 let index = require('./routes/index')
-let twatt = require('./routes/twatt')
+let twatt_recent = require('./routes/twatt-recent')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/api', index)
-app.use('/api/twatt', twatt)
+app.use('/api/twatt', twatt_recent)
 
 app.listen(3000)
 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const router = express.Router()
 const controllers = require('../controllers/twatt-recent')
 
-router.get('/recent', controllers.twitterRecent)
+router.get('/', function (req, res, next) {
+  res.send('Learn twitter recent')
+})
 
 module.exports = router
